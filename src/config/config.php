6f1e5ccf0e37b3bf90f45266a3deadb043a4f1eb
Apoorv Vyas <apvvyas@gmail.com>
2021-09-15 00:33:09 +0530

    first commit

---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+
+    /**
+     * This is the folder which will contain the environment files that are generated to be used in the system
+     */
+    'folder' => 'environments',
+    
+    // This is the file from which the default env Data is supposed to be taken
+    'default_env_data' => '.local.env'
+];
